6a73d00e1d99e47135ac50b874bbd654c9f9ce8e
Tim Jarratt <tjarratt@gmail.com>
2022-11-17 14:57:35 +0100

    Implement method to read input from resource bundle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Suzanne.");
+        List<Integer> input = readInput();
+        for (int i = 0; i < input.size(); i++) {
+            System.out.println(input.get(i));
+        }
+
+        System.out.println("done");
+    }
+
+    private static List<Integer> readInput() {
+        InputStream input = Main.class.getResourceAsStream("/Input");
+        Scanner scanner = new Scanner(input).useDelimiter("\n");
+
+        List<Integer> result = new ArrayList<>();
+        while (scanner.hasNext()) {
+            result.add(Integer.parseInt(scanner.next()));
+        }
+
+        return result;
     }
 }
