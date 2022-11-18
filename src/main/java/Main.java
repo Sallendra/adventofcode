b82dbb4ea3681d4dd68ed931b1711f059b9b45ac
Suzanne TOSUN <suzanne.tosun@backmarket.com>
2022-11-18 18:18:56 +0100

    end of the second steps

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,21 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> input = readInput();
         List<Integer> fuel = calculateFuel(input);
+        List<Integer> recursiveFuel = calculateRecursiveFuel(input);
         Integer sum = 0;
+        Integer recursiveSum = 0;
+        Integer finalSum = 0;
         for (int i = 0; i < fuel.size(); i++) {
             sum = sum + fuel.get(i);
         }
-        System.out.println(sum);
+        for (int i = 0; i < recursiveFuel.size(); i++) {
+            recursiveSum = recursiveSum + recursiveFuel.get(i);
+            System.out.println(recursiveSum);
+        }
+        finalSum = sum +recursiveSum;
+        System.out.println("sum = " + sum);
+        System.out.println("recursive sum = " + recursiveSum);
+        System.out.println("final sum = " + finalSum);
         System.out.println("done");
     }
 
@@ -36,4 +46,18 @@ public class Main {
         }
         return result;
     }
+
+    private static List<Integer> calculateRecursiveFuel(List<Integer> input){
+        List<Integer> restOfFuel = new ArrayList<>();
+        for (int i= 0; i< input.size(); i++){
+            Integer fuel = input.get(i)/3-2;
+            restOfFuel.add(fuel);
+            while ((fuel/3-2)>0){
+                fuel = fuel/3-2;
+                restOfFuel.add(fuel);
+            }
+        }
+        return restOfFuel;
+    }
+
 }
